1a0150ab1489f0477f81f9023dc54d65ba71bda2
Igor Cherednyk <icherednyk@atlassian.com>
2017-03-03 13:27:28 +0200

    Changed TakeScreenShot method.

--- ScreenObjectsHelpers/Helpers/ScreenshotsTaker.cs
+++ ScreenObjectsHelpers/Helpers/ScreenshotsTaker.cs
@@ -3,15 +3,26 @@ using System.Drawing.Imaging;
 using System.Drawing;
 using System.Windows;
 using TestStack.White;
+using System;
 
 namespace ScreenObjectsHelpers.Helpers
 {
-    public class ScreenshotsTaker {
-        public void  TakeScreenShot() {
+    public class ScreenshotsTaker
+    {
+        // name of test should be passed to the method 
+        // to include it to the name of screenshot file
+        // e.g. TakeScreenShot(nameof(<name of test>))
+        public static void TakeScreenShot(string nameOfTest)
+        {
+            var prefix = "Test_";
+            var timestamp = DateTime.Now.ToString("_MM.dd_HHmmss");
+            var extension = ".jpg";
+            var filename = prefix + nameOfTest + timestamp + extension;
+
             ScreenCapture sc = new ScreenCapture();
             // capture entire screen, and save it to a file
             Bitmap img = sc.CaptureScreenShot();
-            img.Save("test.jpg", ImageFormat.Jpeg);
+            img.Save(filename, ImageFormat.Jpeg);
             
         }
     }
