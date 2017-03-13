b06240d8298dc849e5a553544f058655f1157465
Vadym Shevchenko <vshevchenko@atlassian.com>
2017-03-13 18:49:20 +0200

    Find the way to catch Authorization window (where signin in installation). There was problem to find it because this window doen't have title / Name. Also add simple first test to installation Suite.

---
+++ AutomationTestsSolution/Tests/InstallationSuite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.WindowStripControls;
+using TestStack.White.WindowsAPI;
+using TestStack.White;
+using NUnit.Framework;
+using TestStack.White.UIItems.WindowItems;
+using ScreenObjectsHelpers.Helpers;
+using ScreenObjectsHelpers.Windows;
+using ScreenObjectsHelpers.Windows.Options;
+using ScreenObjectsHelpers.Windows.MenuFolder;
+using ScreenObjectsHelpers.Windows.MenuFolder.ActionMenu;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Windows.Automation;
+
+
+
+namespace AutomationTestsSolution.Tests
+{
+    class InstallationSuite : BasicTestInstallation
+    {
+        [Test]
+        public void ContinueButtonIsNotActiveTest()
+        {
+            InstallationWindow installWindow = new InstallationWindow(MainWindow);
+            installWindow.UncheckLicenceAgreementCheckbox();
+
+            bool isContinueButtonActive = installWindow.IsContinueButtonActive();
+            Assert.IsFalse(isContinueButtonActive);
+        }
+
+    }
+}
+
+
