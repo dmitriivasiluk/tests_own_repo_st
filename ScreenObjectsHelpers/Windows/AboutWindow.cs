ea76555f5a8f8750301c0d6e05f071a17b4c031e
SOFTSERVE\dvasy <dmytrovasyliuk96@gmail.com>
2017-03-28 13:03:46 +0300

    Created methods and UI elements for About Window. Added one test to check whether captions are correct on About Window.

--- ScreenObjectsHelpers/Windows/AboutWindow.cs
+++ ScreenObjectsHelpers/Windows/AboutWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
 
 namespace ScreenObjectsHelpers.Windows
@@ -17,5 +18,35 @@ namespace ScreenObjectsHelpers.Windows
             // Need verify opened tab in this method, need implementation! If validation is fail, throw exception!
             Console.WriteLine("WAIT FOR OPENING _ABOUT_ WINDOW");
         }
+        #region UIItems
+        public Label HeaderOfAboutWindow => AboutWindowContainer.Get<Label>(SearchCriteria.ByText("About SourceTree"));
+
+        public Label AppVersion => AboutWindowContainer.Get<Label>(SearchCriteria.ByText("Version 2.0.14.1"));
+
+        public Label CopyrightCaption => AboutWindowContainer.Get<Label>(SearchCriteria.ByText("Copyright Atlassian 2012-2017. All Rights Reserved."));
+
+        public Button CloseAboutWindowButton => AboutWindowContainer.Get<Button>(SearchCriteria.ByAutomationId("CloseWindow"));
+        #endregion
+        
+        #region Methods
+        public string GetHeader()
+        {
+            return HeaderOfAboutWindow.Text;
+        }
+        public string GetAppVersion()
+        {
+            return HeaderOfAboutWindow.Text;
+        }
+        public string GetCopyrightCaption()
+        {
+            return HeaderOfAboutWindow.Text;
+        }
+        public NewTabWindow CloseAboutWindowButtonClick()
+        {
+            CloseAboutWindowButton.Click();
+            return new NewTabWindow(MainWindow);
+        }
+        #endregion
+
     }
 }
