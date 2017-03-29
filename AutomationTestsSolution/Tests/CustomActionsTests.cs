9ea53d88560857aab93b57d4153763874dbd9a5b
SOFTSERVE\dvasy <dmytrovasyliuk96@gmail.com>
2017-03-29 18:59:48 +0300

    Created some methods for custom actions tab. Created "Edit custom actions" window with it's own methods and attributes.

---
+++ AutomationTestsSolution/Tests/CustomActionsTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ScreenObjectsHelpers.Windows;
+using ScreenObjectsHelpers.Windows.Options;
+using ScreenObjectsHelpers.Windows.MenuFolder;
+
+namespace AutomationTestsSolution.Tests
+{
+    class CustomActionsTests : BasicTest
+    {
+
+        [Test]
+        public void AddCustomAction()
+        {
+            NewTabWindow mainWindow = new NewTabWindow(MainWindow);
+            OptionsWindow optionsWindows = mainWindow.OpenMenu<ToolsMenu>().OpenOptions();
+            CustomActionsTab customActionsTab = optionsWindows.OpenTab<CustomActionsTab>();
+            var editCustomActionWindow = customActionsTab.ClickAddCustomActionButton();
+            editCustomActionWindow.SetMenuCaption("test1");
+            editCustomActionWindow.SetScriptToRun("test1");
+        }
+        
+        
+    }
+
+}
+
+
---
+++ ScreenObjectsHelpers/Windows/EditCustomActionWindow.cs
@@ -0,0 +1,72 @@
+﻿using ScreenObjectsHelpers.Windows.Options;
+using System;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace ScreenObjectsHelpers.Windows
+{
+    public class EditCustomActionWindow : GeneralWindow
+    {
+        private UIItemContainer customActionsWindow;
+        private UIItemContainer customActionsTab;
+
+        public EditCustomActionWindow(Window mainWindow, UIItemContainer customActionsTab, UIItemContainer customActionsWindow)
+            : base(mainWindow)
+        {
+            this.customActionsWindow = customActionsWindow;
+            this.customActionsTab = customActionsTab;
+        }
+
+        public override void ValidateWindow()
+        {
+            // Need verify opened tab in this method, need implementation! If validation is fail, throw exception!
+            Console.WriteLine("WAIT FOR OPENING _Edit_Custom_action_Window");
+        }
+
+        #region UIItems
+        public TextBox MenuCaption => customActionsWindow.Get<TextBox>(SearchCriteria.ByAutomationId("CaptionBox"));
+        public CheckBox OpenInASeparateWindow 
+        {
+            get
+            {
+                return customActionsWindow.Get<CheckBox>(SearchCriteria.ByText("Open in a separate window"));
+            }
+        }
+        public CheckBox ShowFullOutput
+        {
+            get
+            {
+                return customActionsWindow.Get<CheckBox>(SearchCriteria.ByText("Show Full Output"));
+            }
+        }
+        public TextBox ScriptToRun => customActionsWindow.Get<TextBox>(SearchCriteria.ByAutomationId(""/*required automation ID*/));
+        public TextBox Parameters => customActionsWindow.Get<TextBox>(SearchCriteria.ByAutomationId(""/*required automation ID*/));
+        public Button OkButton => customActionsWindow.Get<Button>(SearchCriteria.ByText("OK"));
+        public Button CancelButton => customActionsWindow.Get<Button>(SearchCriteria.ByText("Cancel"));
+        #endregion
+
+        #region Methods
+        public void SetMenuCaption(string menuCaption)
+        {
+            MenuCaption.Text = menuCaption;
+        }
+
+        public void SetScriptToRun(string scriptToRun)
+        {
+            SetScriptToRun.Text = scriptToRun;
+        }
+
+        public CustomActionsTab ClickOnOKButton()
+        {
+            OkButton.Click();
+            return new CustomActionsTab(MainWindow, customActionsTab);
+        }
+        public CustomActionsTab ClickOnCancelButton()
+        {
+            CancelButton.Click();
+            return new CustomActionsTab(MainWindow, customActionsTab);
+        }
+        #endregion
+    }
+}
