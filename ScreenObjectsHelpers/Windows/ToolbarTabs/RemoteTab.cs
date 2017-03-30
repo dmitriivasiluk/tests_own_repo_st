26f3d06aa13a9a8de1e2bfabfcfe2c8014db4503
icherednyk <icherednyk@atlassian.com>
2017-03-30 16:37:44 +0300

    Implemented access to toolbar tabs. Implemented access to controls of clone tab (there are still some elements on this tab without access - automationIDs and text properties are not yet configured). Made some refactoring in the project. ***Please note that NewTabWindow should be replaced now with LocalTab as an "entry point" in some tests*** I fixed that in existing OptionsTests.

---
+++ ScreenObjectsHelpers/Windows/ToolbarTabs/RemoteTab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace ScreenObjectsHelpers.Windows.ToolbarTabs
+{
+    public class RemoteTab : NewTabWindow
+    {
+        public RemoteTab(TestStack.White.UIItems.WindowItems.Window mainWindow) : base(mainWindow)
+        {
+        }
+
+        public override UIItem UIElementTab => MainWindow.Get<UIItem>(SearchCriteria.ByText("Remote"));
+    }
+}
