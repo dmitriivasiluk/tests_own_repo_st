30484254551106b566b40dd06a6e839bb13bc288
SOFTSERVE\dvasy <dmytrovasyliuk96@gmail.com>
2017-04-11 16:09:39 +0300

    Created tests for editing and deleting custom actions. Implemented class of confirm deletion dialog window.

--- AutomationTestsSolution/Tests/CustomActionsTests.cs
+++ AutomationTestsSolution/Tests/CustomActionsTests.cs
@@ -19,14 +19,6 @@ namespace AutomationTestsSolution.Tests
             base.SetUp();
         }
 
-        //[TearDown]
-        //public override void TearDown()
-        //{
-        //    base.TearDown();
-        //    var customActionsFilePath = Environment.ExpandEnvironmentVariables(@"%localappdata%\Atlassian\SourceTree\customactions.xml");
-        //    RestoreFile(customActionsFilePath);
-        //}
-        
         [Test]
         public void AddCustomAction()
         {
@@ -34,17 +26,62 @@ namespace AutomationTestsSolution.Tests
             NewTabWindow mainWindow = new NewTabWindow(MainWindow);
             OptionsWindow optionsWindows = mainWindow.OpenMenu<ToolsMenu>().OpenOptions();
             CustomActionsTab customActionsTab = optionsWindows.OpenTab<CustomActionsTab>();
+
             var isMenuCaptionExistsBeforeTest = customActionsTab.IsMenuCaptionExists(testData);
             var editCustomActionWindow = customActionsTab.ClickAddCustomActionButton();
+
             editCustomActionWindow.SetMenuCaption(testData);
             editCustomActionWindow.SetScriptToRun(testData);
             editCustomActionWindow.ClickOnOKButton();
 
             Assert.IsFalse(isMenuCaptionExistsBeforeTest);
             Assert.IsTrue(customActionsTab.IsMenuCaptionExists(testData));
-         }
-     }
+        }
 
+        [Test]
+        public void EditCustomAction()
+        {
+            const string newMenuCaption = "editedCustomAction";
+
+            NewTabWindow mainWindow = new NewTabWindow(MainWindow);
+            OptionsWindow optionsWindows = mainWindow.OpenMenu<ToolsMenu>().OpenOptions();
+            CustomActionsTab customActionsTab = optionsWindows.OpenTab<CustomActionsTab>();
+
+            var isEditCustomActionButtonEnabled = customActionsTab.IsEditCustomActionButtonEnabled();
+            var isMenuCaptionExistsBeforeTest = customActionsTab.IsMenuCaptionExists(newMenuCaption);
+            var editCustomActionWindow = customActionsTab.ClickEditCustomActionButton();
+
+            editCustomActionWindow.SetMenuCaption(newMenuCaption);
+            editCustomActionWindow.ClickOnOKButton();
+
+            Assert.IsFalse(isEditCustomActionButtonEnabled);
+            Assert.IsFalse(isMenuCaptionExistsBeforeTest);
+            Assert.IsTrue(customActionsTab.IsMenuCaptionExists(newMenuCaption));
+        }
+
+        [Test]
+        public void DeleteCustomAction()
+        {
+            const string customActionToBeDeleted = "customActionToBeEdited";
+
+            NewTabWindow mainWindow = new NewTabWindow(MainWindow);
+            OptionsWindow optionsWindows = mainWindow.OpenMenu<ToolsMenu>().OpenOptions();
+            CustomActionsTab customActionsTab = optionsWindows.OpenTab<CustomActionsTab>();
+
+            var isDeleteCustomActionButtonEnabled = customActionsTab.IsDeleteCustomActionButtonEnabled();
+            var isMenuCaptionExistsBeforeTest = customActionsTab.IsMenuCaptionExists(customActionToBeDeleted);
+
+            customActionsTab.ClickDeleteCustomActionButton();
+
+            var confirmDeletionWindow = customActionsTab.SwitchToConfirmDeletionDialogWindow();
+
+            confirmDeletionWindow.ClickOkButton();
+
+            var isMenuCaptionExistsAfterTest = customActionsTab.IsMenuCaptionExists(customActionToBeDeleted);
+
+            Assert.IsFalse(isDeleteCustomActionButtonEnabled);
+            Assert.IsTrue(isMenuCaptionExistsBeforeTest);
+            Assert.IsFalse(isMenuCaptionExistsAfterTest);
+        }
+    }
 }
-
-
--- ScreenObjectsHelpers/Windows/Options/CustomActionsTab.cs
+++ ScreenObjectsHelpers/Windows/Options/CustomActionsTab.cs
@@ -33,6 +33,8 @@ namespace ScreenObjectsHelpers.Windows.Options
         public Button EditCustomActionButton => customActionsTab.Get<Button>(SearchCriteria.ByText("Edit"));
         public Button DeleteCustomActionButton => customActionsTab.Get<Button>(SearchCriteria.ByText("Delete"));
         public ListView AllCustomActions => customActionsTab.Get<ListView>(SearchCriteria.ByClassName("ListView"));
+        public ListViewRow FirstCustomAction => customActionsTab.Get<ListViewRow>(SearchCriteria.ByClassName("ListViewItem"));
+
         #endregion
 
         #region Methods 
@@ -41,6 +43,18 @@ namespace ScreenObjectsHelpers.Windows.Options
             AddCustomActionButton.Click();
             var editCustomActionsWindow = MainWindow.MdiChild(SearchCriteria.ByText("Edit Custom Action"));
             return new EditCustomActionWindow(MainWindow, customActionsTab, editCustomActionsWindow);
+        }
+        public EditCustomActionWindow ClickEditCustomActionButton()
+        {
+            FirstCustomAction.Select();
+            EditCustomActionButton.Click();
+            var editCustomActionsWindow = MainWindow.MdiChild(SearchCriteria.ByText("Edit Custom Action"));
+            return new EditCustomActionWindow(MainWindow, customActionsTab, editCustomActionsWindow);
+        }
+        public void ClickDeleteCustomActionButton()
+        {
+            FirstCustomAction.Select();
+            DeleteCustomActionButton.Click();
         }
         public bool IsMenuCaptionExists(string condition)
         {
@@ -53,7 +67,50 @@ namespace ScreenObjectsHelpers.Windows.Options
                 }
             }
             return false;
-         }
+        }
+        public bool IsEditCustomActionButtonEnabled()
+        {
+            return EditCustomActionButton.Enabled;
+        }
+        public bool IsDeleteCustomActionButtonEnabled()
+        {
+            return DeleteCustomActionButton.Enabled;
+        }
+        public ConfirmDeletionDialogWindow SwitchToConfirmDeletionDialogWindow()
+        {
+            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId("window");
+            var confirmDeletionDialog = this.WaitMdiChildAppears(searchCriteria, 10);
+            return new ConfirmDeletionDialogWindow(customActionsTab, confirmDeletionDialog);
+        }
+        #endregion
+    }
+
+    public class ConfirmDeletionDialogWindow
+    {
+        private readonly UIItemContainer confirmDeletionWindow;
+        private readonly UIItemContainer customActionsTab;
+
+        public ConfirmDeletionDialogWindow(UIItemContainer customActionsTab, UIItemContainer confirmDeletionWindow)
+        {
+            this.customActionsTab = customActionsTab;
+            this.confirmDeletionWindow = confirmDeletionWindow;
+        }
+        #region UIItems
+        public Button CancelButton => confirmDeletionWindow.Get<Button>(SearchCriteria.ByText("Cancel"));
+        public Button OkButton => confirmDeletionWindow.Get<Button>(SearchCriteria.ByText("OK"));
+        #endregion
+
+        #region Methods
+        public UIItemContainer ClickCancelButton()
+        {
+            CancelButton.Click();
+            return customActionsTab;
+        }
+        public UIItemContainer ClickOkButton()
+        {
+            OkButton.Click();
+            return customActionsTab;
+        }
         #endregion
     }
 }
