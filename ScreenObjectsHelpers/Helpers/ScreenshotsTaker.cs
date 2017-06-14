9284456319acb42666b5a90f732ccbbdab310c4b
Dmitry Vasyliuk <dvasyliuk@atlassian.com>
2017-06-14 16:48:07 +0300

    refactoring

--- ScreenObjectsHelpers/Helpers/ScreenshotsTaker.cs
+++ ScreenObjectsHelpers/Helpers/ScreenshotsTaker.cs
@@ -1,18 +1,29 @@
-﻿
-using System.Drawing.Imaging;
-using System.Drawing;
-using System.Windows;
+﻿using System;
 using TestStack.White;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ScreenObjectsHelpers.Helpers
 {
-    public class ScreenshotsTaker {
-        public void  TakeScreenShot() {
+    public class ScreenshotsTaker
+    {
+        // name of test should be passed to the method 
+        // to include it to the name of screenshot file
+        // e.g. TakeScreenShot(nameof(<name of test>))
+        public static void TakeScreenShot(string nameOfTest)
+        {            
+            var prefix = "Test_";
+            var timestamp = DateTime.Now.ToString("_MM.dd_HHmmss");
+            var extension = ".jpg";
+
+            var filename = prefix + nameOfTest + timestamp + extension;
+
+            string path = Environment.ExpandEnvironmentVariables(@"%userprofile%\Documents\");
+
             ScreenCapture sc = new ScreenCapture();
             // capture entire screen, and save it to a file
             Bitmap img = sc.CaptureScreenShot();
-            img.Save("test.jpg", ImageFormat.Jpeg);
-            
+            img.Save(path + filename, ImageFormat.Jpeg);            
         }
     }
 }
